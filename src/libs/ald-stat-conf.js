11181b8f96fd51eb7f48700418f364e6d361ca86
lvzhiming0808 <791679201@qq.com>
2018-06-28 16:20:24 +0800

    会计查询小程序

---
+++ src/libs/ald-stat-conf.js
@@ -0,0 +1,5 @@
+exports.app_key = '01f3ee6b208eddf7fc91ee7ca90e7c38'; //请在此行填写从阿拉丁后台获取的appkey
+exports.getLocation = true; //默认不获取用户坐标位置
+exports.appid = 'wxa0a4ad655459fbd3'; //用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.appsecret = '1d30ac77958e9e86d609ca84580cb3e3';//用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.defaultPath = 'pages/home/index';//小程序的默认首页, 用于分享时path为空时
